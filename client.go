aa13b117400cf9749560ddc006739380d76c921b
Tarcisio da Rocha <tarcisio@dcomp.ufs.br>
2018-12-14 19:34:19 +0000

    Start code

---
+++ client.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+func main() {
+
+	conn, err := net.Dial("tcp", "www.ic.unicamp.br:80")
+	checkError(err)
+
+	_, err = conn.Write([]byte("GET / HTTP/1.0\r\n\r\n"))
+	checkError(err)
+
+	result, err := readFully(conn)
+	checkError(err)
+
+	fmt.Println(string(result))
+
+	os.Exit(0)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func readFully(conn net.Conn) ([]byte, error) {
+	defer conn.Close()
+
+	result := bytes.NewBuffer(nil)
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		result.Write(buf[0:n])
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+	}
+	return result.Bytes(), nil
+}
