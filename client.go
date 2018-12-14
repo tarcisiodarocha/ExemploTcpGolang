39c80bf7ebb5bd05d07174de824ba9ebc45c42b7
Tarcisio da Rocha <tarcisio@dcomp.ufs.br>
2018-12-14 19:42:19 +0000

    Start code

--- client.go
+++ client.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"net"
 	"os"
 )
@@ -12,14 +10,19 @@ func main() {
 
 	conn, err := net.Dial("tcp", "www.ic.unicamp.br:80")
 	checkError(err)
+	defer conn.Close()
 
-	_, err = conn.Write([]byte("GET / HTTP/1.0\r\n\r\n"))
+	_, err = conn.Write([]byte("GET /~afalcao/ HTTP/1.0\r\n\r\n"))
 	checkError(err)
 
-	result, err := readFully(conn)
-	checkError(err)
-
-	fmt.Println(string(result))
+	var buf [512]byte
+	for {
+		n, err := conn.Read(buf[0:])
+		fmt.Print(string(buf[0:n]))
+		if err != nil {
+			break
+		}
+	}
 
 	os.Exit(0)
 }
@@ -29,22 +32,4 @@ func checkError(err error) {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
 	}
-}
-
-func readFully(conn net.Conn) ([]byte, error) {
-	defer conn.Close()
-
-	result := bytes.NewBuffer(nil)
-	var buf [512]byte
-	for {
-		n, err := conn.Read(buf[0:])
-		result.Write(buf[0:n])
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return nil, err
-		}
-	}
-	return result.Bytes(), nil
 }
